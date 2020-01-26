5735ae6213e591feea0ce94e1ecfe6d84e645a97
Serrhii07 <serge070989@gmail.com>
2020-01-26 20:55:43 +0200

    add task-06

---
+++ hw-02/js/task-06.js
@@ -0,0 +1,28 @@
+let input;
+const numbers = [];
+let total = 0;
+
+while (true) {
+  input = prompt('Введите число');
+
+  if (input === null) {
+    break;
+  }
+
+  input = Number(input);
+
+  const notANumber = Number.isNaN(input);
+
+  if (notANumber) {
+    alert('Было введено не число, попробуйте еще раз');
+    continue;
+  }
+
+  numbers.push(input);
+}
+
+for (const number of numbers) {
+  total += number;
+}
+
+console.log(`Общая сумма чисел равна ${total}`);
