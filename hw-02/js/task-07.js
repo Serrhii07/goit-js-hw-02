02c651941e353b59adce096f7721b299086891a5
Serrhii07 <serge070989@gmail.com>
2020-01-29 13:57:18 +0200

    add task-07

---
+++ hw-02/js/task-07.js
@@ -0,0 +1,35 @@
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+const isLoginValid = function(login) {
+  if (login.length >= 4 && login.length <= 16) {
+    return true;
+  }
+  return false;
+};
+
+const isLoginUnique = function(allLogins, login) {
+  for (const loginWord of allLogins) {
+    if (login === loginWord) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const addLogin = function(allLogins, login) {
+  if (isLoginValid(login)) {
+    if (isLoginUnique(allLogins, login)) {
+      logins.push(login);
+      return 'Логин успешно добавлен!';
+    } else {
+      return 'Такой логин уже используется!';
+    }
+  } else {
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
+  }
+};
+
+console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
